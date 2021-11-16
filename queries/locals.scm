9dbe013283bfca52c40e751b903a2fe46301aadc
Princeton Ferro <princetonferro@gmail.com>
2021-11-16 17:48:27 -0500

    add locals query

---
+++ queries/locals.scm
@@ -0,0 +1,11 @@
+; locals.scm
+
+[
+ (method_declaration)
+ (block)
+] @local.scope
+
+(parameter (identifier) @local.definition)
+(assignment (identifier) @local.definition)
+
+(identifier) @local.reference
